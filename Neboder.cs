5e9cbfcdee12a2922db6e05f780bc423afd49773
dinooo <145918377+dindooo@users.noreply.github.com>
2023-12-19 10:48:44 +0100

    Add files via upload

---
+++ Neboder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neboderOcjena
+{
+    internal class Neboder
+    {
+       
+        private float visina;
+        private int brojKatova;
+
+        public Neboder()
+        {
+        }
+
+        public void setVisina(float visina)
+        {
+            this.visina = visina;
+        }
+        public float getVisina()
+        {
+            return this.visina;
+        }
+        public void setBrojKatova(int brojKatova)
+        {
+            this.brojKatova = brojKatova;
+        }
+        public int getBrojKatova()
+        {
+            return this.brojKatova;
+        }
+
+        public Neboder(float visina, int brojKatova)
+        {
+            this.visina = visina;
+            this.brojKatova = brojKatova;
+        }
+
+        public override string ToString()
+        {
+            string ispis = "Visina " + this.getVisina() + " Broj katova " + this.getBrojKatova() + " Prosjek je " + this.getVisina()/this.getBrojKatova();
+            return ispis;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neboderOcjena
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Neboder neboder= new Neboder(200+10, 100+1);
+            Console.WriteLine(neboder.ToString());
+           
+
+            Console.ReadKey();
+        }
+    }
+}
